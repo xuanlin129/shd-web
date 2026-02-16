6813a526ea34a603b36bf0df4300bc31005a58db
xuanlin129 <xuanlin129@users.noreply.github.com>
2026-02-16 07:39:58 +0000

    Deploying to gh-pages from @ xuanlin129/shd-web@ca61c002d852fd9741f61c02acac0e7d5a6f62e5 🚀

---
+++ assets/About-BOAz-95d.js
@@ -0,0 +1,12 @@
+import{G as t,H as e}from"./index-BUR0NB3f.js";function r(){return t.jsx(n,{children:t.jsx("h1",{children:"關於頁"})})}const n=e.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+  & > h1 {
+    font-size: 3rem;
+  }
+`;export{r as default};
